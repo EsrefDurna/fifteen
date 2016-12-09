c1a291bba63a9f0a70f85d3ac3addb134f48db3d
Esref Durna <esrefdurna@users.noreply.github.com>
2016-12-08 19:13:03 -0800

    first commit

---
+++ index.ios.js
@@ -0,0 +1,18 @@
+var React = require('react-native');
+
+var {
+  AppRegistry,
+  } = React;
+
+var Game = require('./src/Game');
+
+
+var Fifteen = React.createClass({
+  render: function() {
+    return (
+      <Game/>
+    );
+  },
+});
+
+AppRegistry.registerComponent('Fifteen', () => Fifteen);
